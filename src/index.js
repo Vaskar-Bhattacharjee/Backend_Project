94bfaac7e4a1ab4efeaba0889a21ec058463dba3
Vaskar-Bhattacharjee <vaskarbhattacharjee03@gmail.com>
2025-05-04 13:45:59 +0600

    add express setting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 //require('dotenv').config({path: './.env'});
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 
 dotenv.config(); 
 
 connectDB()
+.then(()=> 
+app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`Server is running on port:  ${process.env.PORT}`);
+    
+}))
+.catch((err)=> console.error("MongoDB connection error from index: ", err));
 
 
 
