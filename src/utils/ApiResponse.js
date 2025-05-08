e25503e1acf6b7de82fb661948567c906ae2dfca
Vaskar-Bhattacharjee <vaskarbhattacharjee03@gmail.com>
2025-05-08 12:18:05 +0600

    Add utils code

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,14 @@
+class ApiResponse {
+    constructor(
+        statusCode,
+        data,
+        message ="Success",
+        stack = ""
+    ){
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode < 400;
+        this.errors = errors;
+    }
+}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,47 @@
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(
+            requestHandler(req, res, next)
+        ).catch((err) => next(err))
+    }
+}
+
+// const asyncHandler = (requestHandler) => {
+//     (req, res, next) => {
+//         Promise.resolve(
+//             requestHandler(req, res, next)
+//         ).catch((err) => next(err))
+//     }
+// }
+
+
+
+
+// const asyncHandler = (fn) => async(rew, res, next) => {
+//     try {
+//         await fn(rew, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
+
+
+
+
+// const asyncHandler = (fn) => async(req, res, next) => {
+//         try {
+//             await fn(req, res, next);
+//         } catch (error) {
+//             res.status(error.code || 500).json({
+//                 success: false,
+//                 message: error.message
+//             })
+//         }
+//     };
+    
+//     export default asyncHandler;
+
